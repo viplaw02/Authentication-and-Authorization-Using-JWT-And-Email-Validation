94a4a76cd48e839be546287af480213f6da8dd08
Viplaw Sinha <148614032+viplaw02@users.noreply.github.com>
2024-11-01 08:17:22 +0530

    Add files via upload

---
+++ Signup Frontend/src/Logout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Logout.css';
+import axios from 'axios';
+
+function Logout() {
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/logout', {}, {
+      });
+      // `axios` automatically throws an error for non-2xx status codes, so you can assume success if no error is thrown
+      toast.success('Logout successful!');
+      // Optionally redirect to the login page after a delay
+      setTimeout(() => {
+        window.location.href = '/login';
+      }, 2000);
+    } catch (error) {
+      // Show toast for any errors
+      toast.error('Logout failed or an error occurred');
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className='Logout'>
+      <button className='Logout1' onClick={handleLogout}>Logout</button>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </div>
+  );
+}
+
+export default Logout;
